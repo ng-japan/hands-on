3dc5ba111bf79a16c6e8765a13eda59e6fbd33e1
laco0416 <laco0416+github@gmail.com>
2017-01-03 00:02:49 +0900

    feat(tutorial): update cli version to beta.24

--- courses/tutorial/ch-2/e2e/app.po.ts
+++ courses/tutorial/ch-2/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor/globals';
+import { browser, element, by } from 'protractor';
 
-export class Ch1Page {
+export class HelloAngularPage {
   navigateTo() {
     return browser.get('/');
   }

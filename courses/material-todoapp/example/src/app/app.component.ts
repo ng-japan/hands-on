2d69605fc8c4195994978b43a1a3911a2b89cb18
laco0416 <laco0416+github@gmail.com>
2017-01-05 23:31:21 +0900

    feat(material-todoapp): list up todolist

--- courses/material-todoapp/example/src/app/app.component.ts
+++ courses/material-todoapp/example/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './shared/todo';
+import { todoList } from './shared/todo-list';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello Angular!';
+  title = 'Todo List';
+
+  todoList = todoList;
 }

0e0ccbd20cfbb12c41d65318c146c61ea8a78def
Suguru Murakami <sugumura@gmail.com>
2017-09-05 02:56:34 +0900

    Update Angular and CLI versions 1.3.x tutorial/ch-2

--- courses/tutorial/ch-2/src/app/app.component.ts
+++ courses/tutorial/ch-2/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Hero } from './hero';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'Angular!';
 
   hero: Hero = {
     id: 1,
